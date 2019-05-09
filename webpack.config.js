6f7c7809037298d0ea9733ac4feb126b189ae577
Rupertofly <rupertqmoore@me.com>
2019-05-09 14:32:16 +1000

    actual

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    vendor: "./src/index.ts"
+  },
+  mode: "development",
+  devServer: {
+    publicPath: path.join('/dist/'),
+    index: './dist/index.html',
+    port: 5050
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /.tsx?$/,
+        loader:'ts-loader'
+
+      }
+    ]
+  },
+  resolve:{
+    extensions: ['.js','jsx','.ts', '.tsx' ]
+  },
+  externals: [
+    'd3'
+  ],
+  plugins: [],
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname,"dist")
+    
+  },
+}
