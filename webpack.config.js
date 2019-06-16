518e3a73423034f9bc712fb73140c8930acad8ea
Rupert Moore <rupertmoore@ruby-imac.local>
2019-06-16 13:54:55 +1000

    some module updating
    Signed-off-by: Rupert Moore <rupertmoore@ruby-imac.local>

--- webpack.config.js
+++ webpack.config.js
@@ -2,34 +2,31 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    vendor: "./src/index.ts"
+    vendor: './src/index.ts'
   },
-  mode: "development",
+  mode: 'development',
   devServer: {
     publicPath: path.join('/dist/'),
     index: './dist/index.html',
-    port: 5050
+    port: 5050,
+    clientLogLevel: 'warning'
   },
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
   module: {
     rules: [
       {
         test: /.tsx?$/,
-        loader:'ts-loader'
-
+        loader: 'ts-loader'
       }
     ]
   },
-  resolve:{
-    extensions: ['.js','jsx','.ts', '.tsx' ]
+  resolve: {
+    extensions: ['.js', 'jsx', '.ts', '.tsx']
   },
-  externals: [
-    'd3'
-  ],
+  externals: ['d3'],
   plugins: [],
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname,"dist")
-    
-  },
-}
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
